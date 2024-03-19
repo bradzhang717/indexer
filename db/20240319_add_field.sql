cef64530971c45c4c771d8742f6a5b35972f2389
brad <zhtxcster@gmail.com>
2024-03-19 23:20:38 +0800

    add idx_inscription_number index  on table utxos/inscriptions/inscriptions_stats

--- db/20240319_add_field.sql
+++ db/20240319_add_field.sql
@@ -5,13 +5,16 @@ tap_indexer;
 
 ALTER TABLE inscriptions ADD inscription_id varchar(256) NOT NULL DEFAULT ""  COMMENT "inscription id";
 ALTER TABLE inscriptions ADD inscription_number BIGINT NOT NULL DEFAULT 0  COMMENT "inscription number";
+ALTER TABLE `inscriptions` ADD INDEX `idx_inscription_number` (`inscription_number`);
 
 
 ALTER TABLE inscriptions_stats ADD inscription_id varchar(256) NOT NULL DEFAULT ""  COMMENT "inscription id";
 ALTER TABLE inscriptions_stats ADD inscription_number BIGINT NOT NULL DEFAULT 0  COMMENT "inscription number";
+ALTER TABLE `inscriptions_stats` ADD INDEX `idx_inscription_number` (`inscription_number`);
 
 
 ALTER TABLE utxos ADD inscription_id varchar(256) NOT NULL DEFAULT ""  COMMENT "inscription id";
 ALTER TABLE utxos ADD inscription_number BIGINT NOT NULL DEFAULT 0  COMMENT "inscription number";
-
 ALTER TABLE utxos DROP COLUMN sn;
+ALTER TABLE `utxos` ADD INDEX `idx_inscription_number` (`inscription_number`);
+
